b051a4e04e25662a8ab2c1be7f721b0de884db88
spetti <spetti@fas.harvard.edu>
2022-06-18 14:56:52 -0400

    fixed issue with ytrain

--- QTL/joint_qtl_mapping.py
+++ QTL/joint_qtl_mapping.py
@@ -9,6 +9,7 @@ import argparse
 sys.path.insert(0, 'utils/')
 import call_glmnet as mlg
 import localize as lc
+from sklearn.metrics import r2_score
 
 # input data
 parser = argparse.ArgumentParser(description='Run GLMNET and localization')
@@ -24,13 +25,15 @@ parser.add_argument("-l", "--loci_list", dest = "L_file", default = None, help="
 # location for results and intermediate outputs 
 parser.add_argument("-output", "--output_prefix", dest = "output_prefix", default = None, help="where to save results and intermediate steps", type = str)
 
-# load F matrix when starting pipeline at localization steps 1 or 2
-parser.add_argument("-load_F", "--load_F", dest = "F_in", default = None, help="load F; for use when", type = str)
+# load F matrix and predictions when starting pipeline at localization steps 1 or 2
+parser.add_argument("-load_F", "--load_F", dest = "F_in", default = None, help="load F; for use when starting pipeline after first or second run of GLMNET", type = str)
+parser.add_argument("-load_P", "--load_preds", dest = "Preds_in", default = None, help="load predictions; for use when starting pipeline after first or second run of GLMNET (must correspond to same training set)", type = str)
 
 # options with defaults
 parser.add_argument("-ct", "--correlation_threshold", dest = "ct", default = .99, help="include loci with maximum pearson correlation c", type = float)
 parser.add_argument("-lt1", "-round_1_localization_threshold", dest = "lt1", default = 0.0, help="only localize loci with magnitude at least lt1 for some phenotype", type = float)
 parser.add_argument("-lt2", "--round_2_localization_threshold", dest = "lt2", default = .003, help="only localize loci with magnitude at least lt2 for some phenotype", type = float)
+parser.add_argument("-norm", "--norm_to_rank_loci", dest = "norm", default = "l1", help="use norm l1 or l2 to rank loci to localize", type = str)
 parser.add_argument("-v", action='store_true', help="verbose", default = False)
 parser.add_argument("-po", action='store_true', help="compute number of loci that remain with cc threshold",  default = False)
 parser.add_argument("-w", "--width", dest = "width", default = 50, help="max width of confidence intervals", type = int)
@@ -119,10 +122,10 @@ if do_corr:
     if args.po: exit()
     
 L = np.arange(0,GT.shape[1])
-Xtrain = GT[:,loci_to_keep]
-Ytrain = PT
-Xval = GV[:,loci_to_keep]
-Yval = PV
+Xtrain = np.copy(GT)[:,loci_to_keep]
+Ytrain = np.copy(PT)
+Xval = np.copy(GV)[:,loci_to_keep]
+Yval = np.copy(PV)
 for A in [Xtrain,Ytrain, Xval, Yval]:
     if np.isnan(A).any():
         raise ValueError("inputs cannot have NaN")
@@ -137,20 +140,23 @@ if args.v:
 
 if first_run_glmnet_needed:
     if args.v: print("running glmnet round 1")
-    F,first_o_preds = mlg.get_coefs(Xtrain,Ytrain,Xval,Yval)
+    F,preds = mlg.get_coefs(Xtrain,Ytrain,Xval,Yval)
     np.save(open(args.output_prefix+f"/first_F_cc_{args.ct}.npy", 'wb'), F)
-    np.save(open(args.output_prefix+f"/first_preds_cc_{args.ct}.npy", 'wb'), first_o_preds)
+    np.save(open(args.output_prefix+f"/first_preds_cc_{args.ct}.npy", 'wb'), preds)
     if args.v: print(f"first glmnet run finished and results saved at {args.output_prefix}/first_F_cc_{args.ct}.npy")
     if args.fo: exit()
 else:
     F = np.load(args.F_in) 
+    preds = np.load(args.Preds_in)
     if args.v: print(f"loaded F")
 
         
 # run first round of localization
 if args.sl2o==False:
+    computed_preds = Xtrain @ F.T
+    affine_term = (preds-computed_preds)[0,...]
     if args.v: print("running localization round 1")
-    idx_filt, loci_filt, F_ori, X, Y, Ysub, loci = lc.prep_arrays(F, GT, PT, loci_to_keep, args.lt1, args.v)
+    idx_filt, loci_filt, F_ori, X, Y, Ysub, loci = lc.prep_arrays(F, np.copy(GT), np.copy(PT), loci_to_keep, args.lt1, args.v, affine_term, norm = args.norm)
     expanded_list = lc.localize_and_split(idx_filt, loci_filt, F_ori, X, Y, Ysub, loci, args.width, args.std, args.v)
     loci_to_keep = [False for _ in range(GT.shape[1])]
     for _ in expanded_list:
@@ -159,30 +165,41 @@ if args.sl2o==False:
         except:
             print(_, len(loci_to_keep), GT.shape[1])
             raise ValueError("index issue found")
-    np.save(open(args.output_prefix+f"/loci_kept_after_localization_round_1_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}.npy", 'wb'), loci_to_keep)
-    print(f"num of loci identified: {sum(loci_to_keep)}")
+            
+    if args.v: 
+        print(f"num of loci identified: {sum(loci_to_keep)}")
+    print("test")
+    np.save(open(args.output_prefix+f"/loci_kept_after_localization_round_1_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}_norm_{args.norm}.npy", 'wb'), loci_to_keep)
 
         
 # run glmnet with loci selected in first round of localization
 if args.sl2o == False and args.sl1o== False:
-    Xtrain = GT[:,loci_to_keep]
-    Ytrain = PT
-    Xval = GV[:,loci_to_keep]
-    Yval = PV
+    Xtrain = np.copy(GT)[:,loci_to_keep]
+    Ytrain = np.copy(PT)
+    Xval = np.copy(GV)[:,loci_to_keep]
+    Yval = np.copy(PV)
     if args.v: print("running glmnet round 2")
     F, preds = mlg.get_coefs(Xtrain,Ytrain,Xval,Yval)
-    np.save(open(args.output_prefix+f"/second_F_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}.npy", 'wb'), F)
-    np.save(open(args.output_prefix+f"/second_preds_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}.npy", 'wb'), preds)
+    print("R2 time!")
+    for _ in range(Ytrain.shape[1]):
+        print(r2_score(Ytrain[:,_], preds[:,_]))
+    np.save(open(args.output_prefix+f"/second_F_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}_norm_{args.norm}.npy", 'wb'), F)
+    np.save(open(args.output_prefix+f"/second_preds_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}_norm_{args.norm}.npy", 'wb'), preds)
     if args.v: print(f"second glmnet run finished and results saved at {args.output_prefix}/second_F_cc_{args.ct}_lt1_{args.lt1}_width_{args.width}_std_{args.std}.npy")
-        
+    np.save(open(args.output_prefix+"/ytrain.npy","wb"), Ytrain)
+    np.save(open(args.output_prefix+"/F.npy","wb"),F)
+    np.save(open(args.output_prefix+"/preds","wb"),preds)
+    exit()
 
 # run second round of localization
 if args.v: print("running localization round 2")
 print(F.shape)
 print(args.lt2)
-idx_filt, loci_filt, F_ori, X, Y, Ysub, loci = lc.prep_arrays(F, GT, PT, loci_to_keep, args.lt2, args.v)
+computed_preds = Xtrain @ F.T
+affine_term = (preds-computed_preds)[0,...]
+idx_filt, loci_filt, F_ori, X, Y, Ysub, loci = lc.prep_arrays(F,np.copy(GT), np.copy(PT), loci_to_keep, args.lt2, args.v, affine_term, norm = args.norm)
 loci_localized, all_confidence_intervals = lc.compute_interval_intersections(idx_filt, loci_filt, F_ori, X, Y, Ysub, loci, args.width, args.std, args.lt2, args.v)
-pickle.dump((loci_localized, all_confidence_intervals), open(args.output_prefix+f"/CI_after_localization_round_2_cc_{args.ct}_lt2_{args.lt2}_width_{args.width}_std_{args.std}.pickle", 'wb'))
+pickle.dump((loci_localized, all_confidence_intervals), open(args.output_prefix+f"/CI_after_localization_round_2_cc_{args.ct}_lt1_{args.lt1}_lt2_{args.lt2}_width_{args.width}_std_{args.std}_norm_{args.norm}.pickle", 'wb'))
 
     
     
